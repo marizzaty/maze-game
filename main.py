2af82c944f348cdc3f6f5814b36067f6baa12d51
MardhiatulII <74031371+marizzaty@users.noreply.github.com>
2021-10-04 14:55:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+print('''
+88888888888888888888888888888888888888888888888888888888888888888888888
+88.._|      | `-.  | `.  -_-_ _-_  _-  _- -_ -  .'|   |.'|     |  _..88
+88   `-.._  |    |`!  |`.  -_ -__ -_ _- _-_-  .'  |.;'   |   _.!-'|  88
+88      | `-!._  |  `;!  ;. _______________ ,'| .-' |   _!.i'     |  88
+88..__  |     |`-!._ | `.| |_______________||."'|  _!.;'   |     _|..88
+88   |``"..__ |    |`";.| i|_|MMMMMMMMMMM|_|'| _!-|   |   _|..-|'    88
+88   |      |``--..|_ | `;!|l|MMoMMMMoMMM|1|.'j   |_..!-'|     |     88
+88   |      |    |   |`-,!_|_|MMMMP'YMMMM|_||.!-;'  |    |     |     88
+88___|______|____!.,.!,.!,!|d|MMMo * loMM|p|,!,.!.,.!..__|_____|_____88
+88      |     |    |  |  | |_|MMMMb,dMMMM|_|| |   |   |    |      |  88
+88      |     |    |..!-;'i|r|MPYMoMMMMoM|r| |`-..|   |    |      |  88
+88      |    _!.-j'  | _!,"|_|M<>MMMMoMMM|_||!._|  `i-!.._ |      |  88
+88     _!.-'|    | _."|  !;|1|MbdMMoMMMMM|l|`.| `-._|    |``-.._  |  88
+88..-i'     |  _.''|  !-| !|_|MMMoMMMMoMM|_|.|`-. | ``._ |     |``"..88
+88   |      |.|    |.|  !| |u|MoMMMMoMMMM|n||`. |`!   | `".    |     88
+88   |  _.-'  |  .'  |.' |/|_|MMMMoMMMMoM|_|! |`!  `,.|    |-._|     88
+88  _!"'|     !.'|  .'| .'|[@]MMMMMMMMMMM[@] \|  `. | `._  |   `-._  88
+88-'    |   .'   |.|  |/| /                 \|`.  |`!    |.|      |`-88
+88      |_.'|   .' | .' |/                   \  \ |  `.  | `._    |  88
+88     .'   | .'   |/|  /                     \ |`!   |`.|    `.  |  88
+88  _.'     !'|   .' | /                       \|  `  |  `.    |`.|  88
+88 vanishing point 888888888888888888888888888888888888888888888(FL)888
+''')
+print("Welcome to The Maze.")
+print("Your mission is get out ASAP.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Assigning variable 
+first_step = (input("There are two road in front of you. Straight or right? ")).lower()
+
+#The very dangerous journey
+if first_step == "straight":
+  print ("\nThank god nothing happen.\nYou continue your journey.")
+  second_step = (input("You meet a big obstacle rock. Crawl or climb? ")).lower()
+
+  if second_step == "climb":
+    print ("\nThe rock fall over the floor.\nLuckily you choose to climb.")
+    third_step = (input("\nThere are 3 doors. Choose one.\nGolden or Wooden or Silver. ")).lower()
+
+    if third_step == "golden":
+      print ("\nYou managed to get out!!\n Congratulations!")
+
+    elif third_step == "silver":
+      print ("\nLots! Lots of hungry wolves.\nLooks like they able to feed their hunger.\nGAME OVER!")
+
+    else:
+      print ("\nAhh, all your deepest dream came true.\nYou stuck in illusion that you cannot get out.\nGAME OVER!")
+
+  else:
+    print ("\nOpps.. The rock fall over and you squashed to death.\nGAME OVER!")
+
+else:
+  print ("\nThere is a big hole.\nYou fallen to death.\nGAME OVER!")
+
+
